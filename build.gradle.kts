a818ad3f20958fa20e93c9841c997df7d235efdd
derker <16153284+dongkuo@users.noreply.github.com>
2024-05-09 23:48:06 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,3 @@
 plugins {
     kotlin("jvm") version "1.9.23"
 }
-
-dependencies {
-    val ktorVersion = "2.3.10"
-    implementation("org.jsoup:jsoup:1.16.2")
-    implementation("io.ktor:ktor-client-core:$ktorVersion")
-    implementation("io.ktor:ktor-client-okhttp:$ktorVersion")
-    implementation(kotlin("reflect"))
-    implementation("org.slf4j:slf4j-api:2.0.12")
-    testImplementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.23.1")
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
-kotlin {
-    jvmToolchain(17)
-}
